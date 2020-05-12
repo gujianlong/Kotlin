be5eb6800243d3a17eb05f5a63ffe3e766a81f23
gujianlong <gjl2879905137@163.com>
2020-05-12 09:08:49 +0800

    //

--- app/src/main/java/com/bw/myapplication/MainActivity.kt
+++ app/src/main/java/com/bw/myapplication/MainActivity.kt
@@ -8,5 +8,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val kotlin = kotlin()
+        kotlin.score
+        kotlin.checkFace(90)
+        println(kotlin.returnBig(a = 9,b = 6))
     }
 }
---
+++ app/src/main/java/com/bw/myapplication/kotlin.kt
@@ -0,0 +1,29 @@
+package com.bw.myapplication
+
+/*
+ *@auther:谷建龙
+ *@Date: 2020/5/12
+ *@Time:8:37
+ *@Description:
+ * */
+
+
+class kotlin {
+    fun checkFace(score: Int) {
+        if (score > 80)
+            println("这是一个帅哥")
+        else
+            println("这是一个衰哥")
+
+    }
+
+    var score = 90
+
+    fun returnBig(a: Int, b: Int): Int {
+        if (a > b) {
+            return a
+        } else
+            return b
+    }
+
+}
